87b839e3ab35e6518195f8a2bb94e7e204b9a244
Priyansh-Antino <priyansh@antino.com>
2024-08-13 11:12:13 +0530

    Add Graphql User model.

---
+++ src/graphql/models/User.model.ts
@@ -0,0 +1,22 @@
+import { Field, Int, ObjectType } from '@nestjs/graphql';
+
+@ObjectType()
+export class User {
+  @Field((type) => Int) //Specifying it is a number type.
+  id: number;
+
+  @Field()
+  name: string;
+
+  @Field({ nullable: true })
+  profilePhoto?: string; //'?' specifies that it is an optional field. But for graphql, we need ad nullable=true.
+
+  @Field()
+  email: string;
+
+  @Field()
+  password: string;
+
+  @Field((type) => [Int], { defaultValue: [] }) //It will be empty at the start.
+  blogs: number[];
+}
