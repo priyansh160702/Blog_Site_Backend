12bf3cdf78f9f04dd4634456bde2026728a0a83c
Priyansh-Antino <priyansh@antino.com>
2024-08-13 15:42:40 +0530

    Setup TypeORM Configuration and Users model.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,26 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
-import { BlogResolver, UserResolver } from './graphql/resolvers';
+import { User } from './graphql/models';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: 'src/schema.gql', //Actual Graphql schema will be created here.
+    }),
+    TypeOrmModule.forRoot({
+      //Server Config
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '12345678',
+      database: 'blog_site',
+      entities: [User], // TypeORM model.
+      synchronize: true,
     }),
   ],
   controllers: [],
--- src/graphql/models/User.model.ts
+++ src/graphql/models/User.model.ts
@@ -1,19 +1,28 @@
 import { Field, Int, ObjectType } from '@nestjs/graphql';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
+//Will be using the same class for TypeOrm models. TypeORM will ignore other decorators.
+
+@Entity({ name: 'Users' }) //Species that it is a TypeORM Entity.
 @ObjectType()
 export class User {
+  @PrimaryGeneratedColumn() // Column it creates is primary and its value is auto-generated.
   @Field((type) => Int) //Specifying it is a number type.
   id: number;
 
+  @Column() //Specifying it is a column (TypeORM).
   @Field()
   name: string;
 
+  @Column()
   @Field({ nullable: true })
   profilePhoto?: string; //'?' specifies that it is an optional field. But for graphql, we need ad nullable=true.
 
+  @Column()
   @Field()
   email: string;
 
+  @Column()
   @Field()
   password: string;
 
