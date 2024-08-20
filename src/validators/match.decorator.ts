c699f52cff1730402ccae474013a6538397eba8f
Priyansh-Antino <priyansh@antino.com>
2024-08-20 14:25:54 +0530

    Add custom decorator Match.

---
+++ src/validators/match.decorator.ts
@@ -0,0 +1,28 @@
+import {
+  registerDecorator,
+  ValidationArguments,
+  ValidationOptions,
+} from 'class-validator';
+
+export function Match(property: string, validationOptions?: ValidationOptions) {
+  return function (object: Object, propertyName: string) {
+    registerDecorator({
+      name: 'match',
+      target: object.constructor,
+      propertyName: propertyName,
+      options: validationOptions,
+      constraints: [property],
+      validator: {
+        validate(value: any, args: ValidationArguments) {
+          const [relatedPropertyName] = args.constraints;
+          const object = args.object as any;
+          return value === object[relatedPropertyName];
+        },
+        defaultMessage(args: ValidationArguments) {
+          const [relatedPropertyName] = args.constraints;
+          return `${args.property} must match ${relatedPropertyName}`;
+        },
+      },
+    });
+  };
+}
