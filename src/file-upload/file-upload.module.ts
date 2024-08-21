de8d3ae1294ade47c4d084d9e04c379e8ab57880
Priyansh-Antino <priyansh@antino.com>
2024-08-21 11:44:14 +0530

    Provide User and Blog repositories to File Upload module.

--- src/file-upload/file-upload.module.ts
+++ src/file-upload/file-upload.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { FileUploadService } from './file-upload.service';
 import { FileUploadController } from './file-upload.controller';
+import { User } from 'src/graphql/models/User.model';
+import { Blog } from 'src/graphql/models/Blog.model';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User, Blog])],
   controllers: [FileUploadController],
   providers: [FileUploadService],
 })
